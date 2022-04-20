2e2d313b795736a33fee53e9f7603b5edbf65614
enue <otoriden@gmail.com>
2022-04-20 10:52:31 +0900

    Pon, Chi.RedTileCount

--- Assets/Package/Runtime/Command/AfterDiscard.cs
+++ Assets/Package/Runtime/Command/AfterDiscard.cs
@@ -29,6 +29,26 @@ namespace TSKT.Mahjongs.Commands
         }
 
         public (TileType, bool, TileType, bool, TileType, bool) Key => MeldUtil.GetKey(TargetTile, pair.Item1, pair.Item2);
+        public int RedTileCount
+        {
+            get
+            {
+                var result = 0;
+                if (TargetTile.red)
+                {
+                    ++result;
+                }
+                if (pair.Item1.red)
+                {
+                    ++result;
+                }
+                if (pair.Item2.red)
+                {
+                    ++result;
+                }
+                return result;
+            }
+        }
     }
 
     public readonly struct Chi : ICommand<AfterDiscard>
@@ -51,6 +71,26 @@ namespace TSKT.Mahjongs.Commands
             return new CommandResult(Controller.Chi(Executor, 塔子));
         }
         public (TileType, bool, TileType, bool, TileType, bool) Key => MeldUtil.GetKey(TargetTile, 塔子.left, 塔子.right);
+        public int RedTileCount
+        {
+            get
+            {
+                var result = 0;
+                if (TargetTile.red)
+                {
+                    ++result;
+                }
+                if (塔子.Item1.red)
+                {
+                    ++result;
+                }
+                if (塔子.Item2.red)
+                {
+                    ++result;
+                }
+                return result;
+            }
+        }
     }
 
     public readonly struct Kan : ICommand<AfterDiscard>
